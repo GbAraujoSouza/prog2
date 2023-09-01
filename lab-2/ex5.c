ecb5aeb7727899fcc44d6df0fe4ac0dae72ca324
Gabriel de Araujo de Souza <gabriel.souzaaraujo109@gmail.com>
2023-09-01 14:01:42 +0000

    atualizar exercicio 5

--- lab-2/ex5.c
+++ lab-2/ex5.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void printArray(int *p, int size);
+
 int main() {
 
     int n;
-    scanf("%d", &n);
+scanf("%d", &n);
 
     int *v = (int *)calloc(n, sizeof(int));
     for (int i = 0; i < n; i++) {
@@ -16,10 +18,31 @@ int main() {
     for (int i = 0; i < n; i++) {
         *(posicoes + i) = -1;    
     }
-    int k;
+    int *k = posicoes;
     for (int i = 0; i < n; i++) {
-        if (*(v+i) > maior)
+        if (*(v+i) >= maior) {
+            maior = *(v+i);
+            *k = i;
+            k++;
+        }
     }
 
+    printArray(v, n);
+    printf("Maior elemento é %d\n", maior);
+    printf("Nas posições: ");
+    for (int i = 0; i < n; i++) {
+        if (*(posicoes+i) != -1)
+            printf("%d ", *(posicoes+i));
+    }
+    puts("");
+    printArray(posicoes, n);
+
     return 0;
+}
+
+void printArray(int *p, int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", *(p+i));
+    }
+    puts("");
 }
