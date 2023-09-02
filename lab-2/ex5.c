3d6ef18559f135d6f9c7935ff8fc43b0c04455a6
Gabriel de Araujo de Souza <gabriel.souzaaraujo109@gmail.com>
2023-09-02 20:16:21 +0000

    finalizar exercicio 5 - lab 2

--- lab-2/ex5.c
+++ lab-2/ex5.c
@@ -1,48 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void printArray(int *p, int size);
+void printArray(int *a, int size);
+void resetaArray(int *a, int size, int val);
 
 int main() {
 
     int n;
 scanf("%d", &n);
 
+    // ler o array
     int *v = (int *)calloc(n, sizeof(int));
     for (int i = 0; i < n; i++) {
         scanf("%d", v+i);
     }
 
-    int maior = v[0];
+    
     int *posicoes = (int *)calloc(n, sizeof(int));
     for (int i = 0; i < n; i++) {
         *(posicoes + i) = -1;    
     }
+
+    int maior = v[0];
     int *k = posicoes;
-    for (int i = 0; i < n; i++) {
-        if (*(v+i) >= maior) {
+    posicoes[0] = 0;
+    for (int i = 1; i < n; i++) {
+        if (*(v+i) > maior) {
             maior = *(v+i);
-            *k = i;
+            resetaArray(posicoes, n, -1);
+            posicoes[0] = i;
+            k = posicoes;
+        }
+        else if (*(v+i) == maior) {
             k++;
+            *k = i;
         }
     }
 
-    printArray(v, n);
-    printf("Maior elemento é %d\n", maior);
+    printf("Maior numero: %d\n", maior);
     printf("Nas posições: ");
     for (int i = 0; i < n; i++) {
-        if (*(posicoes+i) != -1)
+        if (*(posicoes+i) != -1) {
             printf("%d ", *(posicoes+i));
+        }
     }
     puts("");
-    printArray(posicoes, n);
 
     return 0;
 }
 
-void printArray(int *p, int size) {
+void printArray(int *a, int size) {
     for (int i = 0; i < size; i++) {
-        printf("%d ", *(p+i));
+        printf("%d ", *(a+i));
     }
     puts("");
+}
+
+void resetaArray(int *a, int size, int val) {
+    for (int i = 0; i < size; i++) {
+        *(a+i) = val;
+    }
 }
