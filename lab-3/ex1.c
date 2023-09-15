efa415100888eea231c1ebf1bc7fba3d0b35a15e
Gabriel de Araujo de Souza <gabriel.souzaaraujo109@gmail.com>
2023-09-15 12:19:11 +0000

    exercicio 1 lab3

---
+++ lab-3/ex1.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int L,C,M,N, **plantacao;
+    scanf("%d %d %d %d", &L, &C, &M, &N);
+
+    plantacao = (int **)malloc(L * sizeof(int *));
+    if (!plantacao) {
+        puts("Erro na alocação de memoria!");
+        return -1;
+    }
+
+    for (int linha = 0; linha < L; linha++) {
+        *(plantacao + linha) = (int *)malloc(C * sizeof(int));
+        if (! (*(plantacao + linha))) {
+            puts("Erro na alocação de memoria!");
+            return -1;
+        }
+    }
+
+    for (int linha = 0; linha < L; linha++) {
+        for (int col = 0; col < C; col++) {
+            scanf("%d", *(plantacao + linha) + col);
+        }    
+    }
+
+    int maiorSoma = 0;
+
+    for (int linha = 0; linha < L; linha += M) {
+        for (int col = 0; col < C; col += N) {
+            int somaCorrente = 0;
+            for (int i = 0; i < M; i++) {
+                for (int j = 0; j < N; j++) {
+                    somaCorrente += *(*(plantacao + linha + i) + col + j);
+                }
+            }
+            if (somaCorrente > maiorSoma) {
+                maiorSoma = somaCorrente;
+            }
+        }
+    }
+
+    printf("%d\n", maiorSoma);
+
+    return 0;
+}
