658d4e299957044a81808775e70f0e47ed32fbe4
Gabriel de Araujo de Souza <gabriel.souzaaraujo109@gmail.com>
2023-09-17 17:02:55 +0000

    comentarios ex1 lab 3

--- lab-3/ex1.c
+++ lab-3/ex1.c
@@ -6,12 +6,12 @@ int main() {
     int L,C,M,N, **plantacao;
     scanf("%d %d %d %d", &L, &C, &M, &N);
 
+    // inicialização da matriz como ponteiro de ponteiros
     plantacao = (int **)malloc(L * sizeof(int *));
     if (!plantacao) {
         puts("Erro na alocação de memoria!");
         return -1;
     }
-
     for (int linha = 0; linha < L; linha++) {
         *(plantacao + linha) = (int *)malloc(C * sizeof(int));
         if (! (*(plantacao + linha))) {
@@ -20,14 +20,15 @@ int main() {
         }
     }
 
+    // Leitura da matriz
     for (int linha = 0; linha < L; linha++) {
         for (int col = 0; col < C; col++) {
             scanf("%d", *(plantacao + linha) + col);
         }    
     }
 
+    // varredura da matriz por lote para achar a maior soma possivel
     int maiorSoma = 0;
-
     for (int linha = 0; linha < L; linha += M) {
         for (int col = 0; col < C; col += N) {
             int somaCorrente = 0;
