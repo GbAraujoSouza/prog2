607b27c2c5091b2c57800b15d855a2575bd49832
Gabriel de Araujo de Souza <gabriel.souzaaraujo109@gmail.com>
2023-10-06 20:46:56 +0000

    lab 5 files

--- lab-5/ex3.c
+++ lab-5/ex3.c
@@ -10,17 +10,27 @@ typedef struct
 int main() {
 
     FILE* pArquivo;
-    if (!(pArquivo = fopen("./alunos.dat", "rb"))) {
+    if (!(pArquivo = fopen("./alunos.dat", "rb+"))) {
         puts("Erro ao abrir arquivo.");
         return -1;
     }
 
+    int mediasAlteradas = 0, totalAlunos = 0;
+
     while (!feof(pArquivo))
     {
         puts("contagem");
-        Taluno a;
-        if (fread(&a, sizeof(Taluno), 1, pArquivo)){
-            printf("%s P1: %.2lf P2: %.2lf P3: %.2lf Media%.2lf \n", a.nome, a.nota[0], a.nota[1], a.nota[2], a.media);
+        Taluno aluno;
+        if (fread(&aluno, sizeof(Taluno), 1, pArquivo)){
+            fseek(pArquivo, sizeof(int), SEEK_SET);
+            if (aluno.media != (0.25 * aluno.nota[0] + 0.35 * aluno.nota[1] + 0.4 * aluno.nota[2])) {
+                aluno.media = (0.25 * aluno.nota[0] + 0.35 * aluno.nota[1] + 0.4 * aluno.nota[2]);
+                fwrite(&aluno.media, sizeof(int), 1, pArquivo);
+                mediasAlteradas++;
+            }
+            totalAlunos++;
+
+            printf("%s P1: %.2lf P2: %.2lf P3: %.2lf Media%.2lf \n", aluno.nome, aluno.nota[0], aluno.nota[1], aluno.nota[2], aluno.media);
         }
     }
     puts("");
